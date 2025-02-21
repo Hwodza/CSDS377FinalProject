01e0508fd7f77f8324f03add8211f44634cd692a
Henry <hwodza@gmail.com>
2025-02-21 21:15:18 +0000

    most of part 3

--- Web/lampisite/lampisite/urls.py
+++ Web/lampisite/lampisite/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('lampi/', include('lampi.urls')),
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name="lampi/login.html"), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name="lampi/logout.html", next_page="/"), name="logout"),
     path('', include('lampi.urls')),
 ]
