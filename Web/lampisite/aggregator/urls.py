5b41d6e27d6cb51eaa3e321dc6cffd8fcd4c78b1
Henry Odza <64870011+Hwodza@users.noreply.github.com>
2025-04-09 14:08:57 -0400

    Added app name

--- Web/lampisite/aggregator/urls.py
+++ Web/lampisite/aggregator/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'aggregator'
+
 urlpatterns = [
     path('', views.latest_message, name='latest_message'),
     path('latest-message/json/', views.latest_message_json,
