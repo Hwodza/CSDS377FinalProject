2776dbbecac468af46df7b7521276c47a1af7baf
Henry Odza <64870011+Hwodza@users.noreply.github.com>
2025-04-09 14:02:04 -0400

    website should pull data

--- Web/lampisite/aggregator/urls.py
+++ Web/lampisite/aggregator/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.latest_message, name='latest_message'),
+    path('latest-message/json/', views.latest_message_json,
+         name='latest_message_json'),
 ]
--- Web/lampisite/aggregator/views.py
+++ Web/lampisite/aggregator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import SystemData
+from django.http import JsonResponse
 
 
 def latest_message(request):
@@ -9,3 +10,16 @@ def latest_message(request):
     except SystemData.DoesNotExist:
         context = {'message': None}
     return render(request, 'aggregator/latest_message.html', context)
+
+
+def latest_message_json(request):
+    try:
+        message = SystemData.objects.latest('timestamp')
+        data = {
+            'topic': message.topic,
+            'payload': message.payload,
+            'timestamp': message.timestamp.strftime('%Y-%m-%d %H:%M:%S'),
+        }
+    except SystemData.DoesNotExist:
+        data = {'error': 'No messages received yet.'}
+    return JsonResponse(data)
