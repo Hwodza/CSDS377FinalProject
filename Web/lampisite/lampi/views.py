9814c6fd1148a9e8ca4ad092b6122a6b0e345e01
Henry Odza <64870011+Hwodza@users.noreply.github.com>
2025-04-27 19:46:44 -0400

    added chartjs, no clue if it works

--- Web/lampisite/lampi/views.py
+++ Web/lampisite/lampi/views.py
@@ -1,6 +1,8 @@
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from datetime import timedelta
 from .models import Lampi, SenderDevice, DeviceData, DiskStats, CpuLoad, \
 NetworkStats
 from django.conf import settings
@@ -74,12 +76,67 @@ class AddSenderView(LoginRequiredMixin, generic.FormView):
         return super(AddSenderView, self).form_valid(form)
 
 
+# class SenderDetailView(LoginRequiredMixin, generic.TemplateView):
+#     template_name = 'lampi/sender_detail.html'
+
+#     def get_context_data(self, **kwargs):
+#         context = super(SenderDetailView, self).get_context_data(**kwargs)
+#         context['device'] = get_object_or_404(
+#             SenderDevice, pk=kwargs['device_id'], user=self.request.user)
+#         context['MIXPANEL_TOKEN'] = settings.MIXPANEL_TOKEN
+#         return context
+
 class SenderDetailView(LoginRequiredMixin, generic.TemplateView):
     template_name = 'lampi/sender_detail.html'
 
     def get_context_data(self, **kwargs):
         context = super(SenderDetailView, self).get_context_data(**kwargs)
-        context['device'] = get_object_or_404(
+        device = get_object_or_404(
             SenderDevice, pk=kwargs['device_id'], user=self.request.user)
-        context['MIXPANEL_TOKEN'] = settings.MIXPANEL_TOKEN
+
+        # Get the latest data point
+        current_stats = device.data_points.order_by('-timestamp').first()
+
+        # Get data for charts (last 24 hours)
+        time_threshold = timezone.now() - timedelta(hours=24)
+        chart_data = device.data_points.filter(
+            timestamp__gte=time_threshold
+        ).order_by('timestamp')
+
+        # Prepare chart data
+        chart_context = {
+            'timestamps': [data.timestamp.isoformat() for data in chart_data],
+            'memory_data': {
+                'free': [data.kbmemfree for data in chart_data],
+                'used': [data.kbmemused for data in chart_data],
+                'percent': [data.memused_percent for data in chart_data],
+            },
+            'cpu_temp': [data.cputemp for data in chart_data],
+            'disk_stats': [],
+            'cpu_loads': [],
+            'network_stats': []
+        }
+
+        # Add related data if current_stats exists
+        if current_stats:
+            chart_context['disk_stats'] = [
+                {'device': stat.device, 'wait': stat.wait, 'util': stat.util}
+                for stat in current_stats.disk_stats.all()
+            ]
+            chart_context['cpu_loads'] = [
+                {'core': load.core, 'load': load.load}
+                for load in current_stats.cpu_loads.all()
+            ]
+            chart_context['network_stats'] = [
+                {'iface': stat.iface, 'rx_kb': stat.rx_kb, 'tx_kb': stat.tx_kb}
+                for stat in current_stats.network_stats.all()
+            ]
+
+        context.update({
+            'device': device,
+            'current_stats': current_stats,
+            'chart_data': chart_context,
+            'MIXPANEL_TOKEN': settings.MIXPANEL_TOKEN,
+        })
+
         return context
